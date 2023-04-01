9d3b7d5d094142838bb2c4a827817528dcce5002
Pere Mato <pere.mato@cern.ch>
2023-04-01 11:41:22 +0200

    Keep the original structure

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -21,6 +21,7 @@ sources = [
 script = raw"""
 
 cmake ${WORKSPACE}/srcdir -B build \
+    -DJulia_PREFIX=${prefix} \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
